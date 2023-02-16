bc6174956f06f17fcadd48b21eaf0ddb1cf2cbb8
Thaddaeus <thaddaeusotieno@gmail.com>
2023-02-16 08:56:00 +0300

    /

--- array.js
+++ array.js
@@ -86,3 +86,109 @@ let index = languages.indexOf("JavaScript");
 console.log(index);
 
 // Output: 1
+
+//Join Method
+
+let message = ["JavaScript", "is", "fun."];
+
+// join all elements of array using space
+let joinedMessage = message.join(" ");
+console.log(joinedMessage);
+
+// Output: JavaScript is fun.
+
+//Length
+let city = ["California", "Barcelona", "Paris", "Kathmandu"];
+
+// find the length of the city array
+let len = city.length;
+console.log(len);
+
+// Output: 4
+
+//Map
+let numbers = [2, 4, 6, 8, 10];
+
+// function to return the square of a number
+function square(number) {
+  return number * number;
+}
+
+// apply square() function to each item of the numbers list
+let square_numbers = numbers.map(square);
+console.log(square_numbers);
+
+// Output: [ 4, 16, 36, 64, 100 ]
+
+//Push
+let city = ["New York", "Madrid", "Kathmandu"];
+
+// add "London" to the array
+city.push("London");
+
+console.log(city);
+
+// Output: [ 'New York', 'Madrid', 'Kathmandu', 'London' ]
+
+//Reduce
+
+const message = ["JavaScript ", "is ", "fun."];
+
+// function to join each string elements
+function joinStrings(accumulator, currentValue) {
+  return accumulator + currentValue;
+}
+
+// reduce join each element of the string
+let joinedString = message.reduce(joinStrings);
+console.log(joinedString);
+
+// Output: JavaScript is fun.
+
+//Reverse 
+let numbers = [1, 2, 3, 4, 5];
+
+// reversing the numbers array
+let reversedArray = numbers.reverse();
+
+console.log(reversedArray);
+
+// Output: [ 5, 4, 3, 2, 1 ]
+
+//Shift ()
+let languages = ["English", "Java", "Python", "JavaScript"];
+
+// removes the first element of the array
+let first = languages.shift();
+console.log(first);
+console.log(languages);
+
+// Output: English
+//         [ 'Java', 'Python', 'JavaScript' ]
+
+//Splice()
+let numbers = [2, 3, 5, 7, 11, 13, 17];
+
+// create another array by slicing numbers from index 3 to 5
+let newArray = numbers.slice(3, 6);
+console.log(newArray);
+
+// Output: [ 7, 11, 13 ]
+
+//Unshift()
+let languages = ["Java", "Python", "C"];
+
+// add "JavaScript" at the beginning of the array
+languages.unshift("JavaScript");
+console.log(languages);
+
+// Output: [ 'JavaScript', 'Java', 'Python', 'C' ]
+
+//Pop()
+let cities = ["Madrid", "New York", "Kathmandu", "Paris"];
+
+// remove the last element
+let removedCity = cities.pop();
+
+console.log(cities)         // ["Madrid", "New York", "Kathmandu"]
+console.log(removedCity);   // Paris
